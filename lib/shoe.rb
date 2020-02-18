d7a8cc96b4818a13a5a8439c0d9f88fed05fcc09
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-18 18:10:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material
+  attr_reader :condition
+ 
+  def initialize(title)
+    @title = title
+  end
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+end
