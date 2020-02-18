ea773c8dc2652905a7ba5e28d4fc89dc6562da64
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-18 18:11:13 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand, :color, :size, :material
-  attr_reader :condition
+  attr_accessor :brand, :color, :size, :material, :condition
+  attr_reader :cobble
  
   def initialize(title)
     @title = title
